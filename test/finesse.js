4e61cc45295b03813648412e8a7ab4f53b9ad1d9
Coty Condry <ccondry@cisco.com>
2020-10-22 00:46:26 -0500

    add finesse test for wrap up reasons

--- test/finesse.js
+++ test/finesse.js
@@ -3,13 +3,16 @@ const finesse = require('../src/models/finesse')
 console.log('process.env.FINESSE_HOST', process.env.FINESSE_HOST)
 console.log('process.env.FINESSE_USER', process.env.FINESSE_USER)
 console.log('process.env.FINESSE_PASSWORD', process.env.FINESSE_PASSWORD)
+console.log('process.env.CUMULUS_MAIN_TEAM_ID', process.env.CUMULUS_MAIN_TEAM_ID)
 
 // copyLayoutConfig(5014, 5027).catch(e => console.log('copylayoutconfig function error:', e))
 main().catch(e => console.log('main function error:', e.message))
 
 async function main () {
-  const allTeams = await finesse.list('Team')
-  console.log('allTeams', allTeams)
+  // const allTeams = await finesse.list('Team')
+  // console.log('allTeams', allTeams)
+  const config = await finesse.getFromTeam(process.env.CUMULUS_MAIN_TEAM_ID, 'WrapUpReasons')
+  console.log('config', config)
 }
 
 // copy Finesse layout XML config from team ID "from" to team ID "to"
@@ -22,4 +25,3 @@ async function copyLayoutConfig (from, to) {
     throw e
   }
 }
-
