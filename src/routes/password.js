fd42dcf5edbef4d7ed8f6d03e0eb776bdd7e2e5c
Coty Condry <ccondry@cisco.com>
2021-06-10 10:35:20 -0500

    use sliced username when resetting VPN password

--- src/routes/password.js
+++ src/routes/password.js
@@ -5,12 +5,17 @@ const ldap = require('../models/ldap')
 // reset password
 router.post('/', async function (req, res) {
   try {
+    // ldap username cannot be more than 20 characters
+    let username = req.user.username
+    if (username.length > 20) {
+      username = req.user.username.slice(0, 20)
+    }
     // set user VPN password in LDAP
     await ldap.resetPassword({
       adminDn: process.env.LDAP_ADMIN_DN,
       adminPassword: process.env.LDAP_ADMIN_PASSWORD,
       newPassword: req.body.password,
-      username: req.user.username
+      username
     })
     return res.status(200).send()
   } catch (e) {
